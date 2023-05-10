4dbe2891728f628f1c17c5cf070ef09df1094606
xmqywx <xmqywx@gmail.com>
2023-05-10 18:20:00 +0800

    config: s3

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -52,7 +52,7 @@ export default {
       // 上传模式 本地上传或云存储
       mode: MODETYPE.LOCAL,
       // 本地上传 文件地址前缀，当且仅当mode为LOCAL时生效
-      domain: 'http://52.65.93.81/api/',
+      domain: 'http://13.54.137.62/api/',
       // domain: 'http://192.168.0.102:8001',
     },
   } as CoolConfig,
