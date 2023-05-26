ea9ffb444b342d1a42b079814fb0adba4cbd9ac6
xmqywx <xmqywx@gmail.com>
2023-05-26 18:44:39 +0800

    secondary person sql fields length

--- src/modules/secondaryPerson/entity/profile.ts
+++ src/modules/secondaryPerson/entity/profile.ts
@@ -7,30 +7,30 @@ import {Column, } from 'typeorm';
  */
 @EntityModel('secondary_person_profile')
 export class SecondaryPersonEntity extends BaseEntity {
-  @Column({ comment: 'person meeting for the car', length: 100 })
+  @Column({ comment: 'person meeting for the car', nullable: true  })
   personName: string;
 
-  @Column({ comment: 'person phone number', length: 20 })
+  @Column({ comment: 'person phone number', nullable: true })
   personPhone: string;
 
-  @Column({ comment: 'person email address', length: 100 })
+  @Column({ comment: 'person email address', nullable: true  })
   personEmail: string;
 
-  @Column({ comment: 'person driver license number', length: 50 })
+  @Column({ comment: 'person driver license number', nullable: true  })
   personLicense: string;
 
-  @Column({ comment: 'person ABN number', length: 50 })
+  @Column({ comment: 'person ABN number', nullable: true  })
   personABN: string;
 
-  @Column({ comment: 'person residential address', length: 200 })
+  @Column({ comment: 'person residential address',nullable: true })
   personAddress: string;
 
-  @Column({ comment: 'person workshop or house', length: 100 })
+  @Column({ comment: 'person workshop or house',nullable: true })
   personLocation: string;
 
-  @Column({ comment: 'authority of sale/relation to the car owner', length: 100 })
+  @Column({ comment: 'authority of sale/relation to the car owner',nullable: true })
   personRelation: string;
 
-  @Column({ comment: 'person secondary phone number', length: 20 })
+  @Column({ comment: 'person secondary phone number',nullable: true  })
   personSecNumber: string;
 }
