bad3ce5e7f93cf6c8dbb20cd0c47126d8807f9a6
xmqywx <xmqywx@gmail.com>
2023-07-26 17:42:24 +0800

    send email pdf

--- src/modules/sendEmail/sendMailToGetDocs.ts
+++ src/modules/sendEmail/sendMailToGetDocs.ts
@@ -4,6 +4,8 @@ const nodemailer = require('nodemailer');
 // const fs = require('fs');
 const dotenv = require('dotenv');
 const envFile = process.env.NODE_ENV === 'prod' ? '.env.production' : '.env.local';
+const pdf = require('html-pdf-chrome');
+
 dotenv.config({ path: envFile });
 
 const fromEmail = process.env.NODE_MAIL_USER;
@@ -27,60 +29,120 @@ export default async function getDocs({ email, name, token, textToSend, giveUplo
     if (email != null) {
         toEmail = email;
     }
+    const html = `<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <style>
+        main {
+          width: 500px;
+          margin: auto;
+        }
+    
+        .to-upload a {
+          /* display: block; */
+          float: left;
+          line-height: 50px;
+          padding: 0px 20px;
+          color: #FFF;
+          font-weight: 500;
+          background-color: chocolate;
+          text-decoration: none;
+        }
+    
+        a:hover {
+          opacity: 0.8;
+        }
+    
+        p {
+          text-indent: 2em;
+        }
+    
+        .to-upload {}
+      </style>
+    </head>
+    
+    <body>
+      <main>
+        <h3>Dear ${name}.</h3>
+        <div>${textToSend}</div>
+        ${
+          giveUploadBtn ? `<p>Please click here to upload some proof documents.</p>
+          <div class="to-upload">
+            <a href="http://13.54.137.62/customer_provide_files?token=${token}">TO UPLOAD</a>
+          </div>` : ''
+        }
+      </main>
+    </body>
+    </html>`;
+    const options = {
+      landscape: true,
+      format: 'A4',
+    };
+    const htmlPdf = await pdf.create(html, options);
+    const pdfBuffer = await htmlPdf.toBuffer();
     const mailOptions = {
         from: fromEmail,
         to: toEmail,
         subject: 'Please provide some proof materials.',
         text: `Dear ${name}.Please click here to upload some proof documents.`,
-        html: `<html lang="en">
+        attachments: [
+          {
+            filename: 'document.pdf',
+            content: pdfBuffer, // 传入 PDF 的二进制数据
+            contentType: 'application/pdf'
+          }
+        ],
+        // html: `<html lang="en">
 
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
-          <style>
-            main {
-              width: 500px;
-              margin: auto;
-            }
+        // <head>
+        //   <meta charset="UTF-8">
+        //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        //   <title>Document</title>
+        //   <style>
+        //     main {
+        //       width: 500px;
+        //       margin: auto;
+        //     }
         
-            .to-upload a {
-              /* display: block; */
-              float: left;
-              line-height: 50px;
-              padding: 0px 20px;
-              color: #FFF;
-              font-weight: 500;
-              background-color: chocolate;
-              text-decoration: none;
-            }
+        //     .to-upload a {
+        //       /* display: block; */
+        //       float: left;
+        //       line-height: 50px;
+        //       padding: 0px 20px;
+        //       color: #FFF;
+        //       font-weight: 500;
+        //       background-color: chocolate;
+        //       text-decoration: none;
+        //     }
         
-            a:hover {
-              opacity: 0.8;
-            }
+        //     a:hover {
+        //       opacity: 0.8;
+        //     }
         
-            p {
-              text-indent: 2em;
-            }
+        //     p {
+        //       text-indent: 2em;
+        //     }
         
-            .to-upload {}
-          </style>
-        </head>
+        //     .to-upload {}
+        //   </style>
+        // </head>
         
-        <body>
-          <main>
-            <h3>Dear ${name}.</h3>
-            <div>${textToSend}</div>
-            ${
-              giveUploadBtn ? `<p>Please click here to upload some proof documents.</p>
-              <div class="to-upload">
-                <a href="http://13.54.137.62/customer_provide_files?token=${token}">TO UPLOAD</a>
-              </div>` : ''
-            }
-          </main>
-        </body>
+        // <body>
+        //   <main>
+        //     <h3>Dear ${name}.</h3>
+        //     <div>${textToSend}</div>
+        //     ${
+        //       giveUploadBtn ? `<p>Please click here to upload some proof documents.</p>
+        //       <div class="to-upload">
+        //         <a href="http://13.54.137.62/customer_provide_files?token=${token}">TO UPLOAD</a>
+        //       </div>` : ''
+        //     }
+        //   </main>
+        // </body>
         
-        </html>`
+        // </html>`
     };
     try {
         const info = await transport.sendMail(mailOptions);
